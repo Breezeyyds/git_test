8f2b3a91a477b3ae4ea355be125dadd740b5eec5
Breeze <Breeze@liangying.com>
2023-03-15 23:50:47 +0800

    first commit by hot-fix

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         // write your code here fan
         System.out.println("hello.git");
+        System.out.println("hot-fix");
 
     }
 }
